467ed222f8b27d026c9a0d0e2480a63b021c16d0
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2019-04-04 14:34:30 +0200

    JPERF-445: Expect `SshUbuntuContainer` to be customizable

--- src/test/kotlin/com/atlassian/performance/tools/sshubuntu/api/ssh/SshUbuntuTest.kt
+++ src/test/kotlin/com/atlassian/performance/tools/sshubuntu/api/ssh/SshUbuntuTest.kt
@@ -5,8 +5,11 @@ import com.atlassian.performance.tools.ssh.api.SshConnection
 import com.atlassian.performance.tools.ssh.api.SshHost
 import com.atlassian.performance.tools.ssh.api.auth.PublicKeyAuthentication
 import com.atlassian.performance.tools.sshubuntu.api.SshUbuntuContainer
-import org.assertj.core.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import org.testcontainers.containers.GenericContainer
+import java.net.Socket
+import java.util.function.Consumer
 
 class SshUbuntuTest {
     @Test
@@ -30,5 +33,32 @@ class SshUbuntuTest {
                 return connection.execute(cmd)
             }
         }
+    }
+
+    @Test
+    fun shouldExposeAdditionalPorts() {
+        val additionalPort = 8080
+        val customization = Consumer { container: GenericContainer<*> ->
+            container.addExposedPort(additionalPort)
+        }
+
+        SshUbuntuContainer(customization).start().use { sshUbuntu ->
+            val ip = sshUbuntu.container.getContainerIpAddress()
+            val port = sshUbuntu.container.getMappedPort(additionalPort)
+            Socket(ip, port).close()
+        }
+    }
+
+    @Test
+    fun shouldRunInPrivilegedMode() {
+        val customization = Consumer { container: GenericContainer<*> ->
+            container.setPrivilegedMode(true)
+        }
+
+        val privileged = SshUbuntuContainer(customization).start().use { sshUbuntu ->
+            sshUbuntu.container.isPrivilegedMode()
+        }
+
+        assertThat(privileged).isTrue()
     }
 }
